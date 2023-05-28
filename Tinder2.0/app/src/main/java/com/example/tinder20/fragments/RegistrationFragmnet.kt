9984b604ac2ed0888cf810f797dc48083ef3b182
PevnevIlya <ipevnevbk.ru@gmail.com>
2023-05-28 11:46:06 +0300

    Добавлена логика выбора пола при регистрации

--- Tinder2.0/app/src/main/java/com/example/tinder20/fragments/RegistrationFragmnet.kt
+++ Tinder2.0/app/src/main/java/com/example/tinder20/fragments/RegistrationFragmnet.kt
@@ -9,9 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.tinder20.R
+import com.example.tinder20.classes.GENDERS
 import com.example.tinder20.databinding.FragmentRegistrationFragmnetBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -19,30 +21,39 @@ class RegistrationFragmnet : Fragment() {
 
     private lateinit var binding: FragmentRegistrationFragmnetBinding
     private var db = Firebase.firestore
+    var gender = GENDERS.UNKNOWN
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRegistrationFragmnetBinding.inflate(inflater, container, false)
 
+
+        binding.maleRadioButton.setOnCheckedChangeListener{
+                _, isChecked ->
+            if (isChecked) {
+                gender = GENDERS.MALE
+            }
+        }
+        binding.femaleRadioButton.setOnCheckedChangeListener{
+                _, isChecked ->
+            if (isChecked) {
+                gender = GENDERS.FEMALE
+            }
+        }
+
         binding.confirm.setOnClickListener{
+            val usersCollection = db.collection("users")
             val dBemail = binding.email.text.toString().trim()
             val dBpassword = binding.password.text.toString().trim()
 
             val userMap = hashMapOf(
                 "email" to dBemail,
-                "password" to dBpassword
+                "password" to dBpassword,
+                "gender" to gender.name
             )
 
-            db.collection("user").add(userMap)
-                .addOnSuccessListener {
-                    Toast.makeText(requireContext(), "Succes", Toast.LENGTH_LONG).show()
-                    binding.email.text.clear()
-                    binding.password.text.clear()
-                }
-                .addOnFailureListener{
-                    Toast.makeText(requireContext(), "Failed", Toast.LENGTH_LONG).show()
-                }
+            usersCollection.add(userMap)
         }
 
         return binding.root
